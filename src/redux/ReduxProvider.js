18454d64f15fe4c5ab5c9c32067e9f6c5fc5ce73
adrian <ablazleon@ono.com>
2018-12-04 14:32:46 +0100

    4. 1. Adding redux: add react redux

---
+++ src/redux/ReduxProvider.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import GlobalState from './reducers';
+import { createStore } from 'redux';
+
+import React from 'react';
+import App from '../App';
+
+export default class ReduxProvider extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+
+}
